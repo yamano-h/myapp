167197e7c8b8a0992821c796b4d2e5fb5cb16479
yamano_h <yamano_h@daitotools.com>
2019-04-08 20:32:35 +0900

    STEP3

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
